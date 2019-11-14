06b89eb20ed91831364e800d1453f110586a62b8
jc <1144096387@qq.com>
2019-11-14 20:39:10 +0800

    引入view ui并初步创建了文件夹

--- src/main.js
+++ src/main.js
@@ -6,6 +6,18 @@ import './plugins/iview.js'
 
 Vue.config.productionTip = false
 
+// 全局配置ViewUI组件的一些属性
+Vue.use(ViewUI, {
+  transfer: true,
+  size: 'large',
+  capture: false,
+  select: {
+      arrow: 'md-arrow-dropdown',
+      arrowSize: 20
+  }
+});
+
+
 new Vue({
   router,
   store,
